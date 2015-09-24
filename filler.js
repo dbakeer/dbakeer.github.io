6f8b703fa8312214b2df60b9c9eb567412b34763
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-09-24 18:57:42 -0400

    the agony and ecstacy of javascript

--- filler.js
+++ filler.js
@@ -71,6 +71,24 @@ $("#one").click(function() {
 });
 }
 
-// determines a win
+// fleshing out the game board
+var grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var gridPositions = function () {
+  grid[0] = $('#zero').html();
+  grid[1] = $('#one').html();
+  grid[2] = $('#two').html();
+  grid[3] = $('#three').html();
+  grid[4] = $('#four').html();
+  grid[5] = $('#five').html();
+  grid[6] = $('#six').html();
+  grid[7] = $('#seven').html();
+  grid[8] = $('#eight').html();
+};
 
-// determines a tie
+
+
+// X player moves
+
+  // $('.grid').on("click", function (){
+  //   $('<div>X</div>').appendTo('#zero')
+  // });
--- project1/ttt.js
+++ project1/ttt.js
@@ -1,31 +1,51 @@
-// setting up information input for Player X
-$(document).ready( function () {
+var playerX = "X";
+var playerY = "Y";
 
-  var playerX = {
-    name: prompt("Please enter your name, Player X"),
-    score: 0,
-    win: 0,
-    lose: 0
+var newGame = function () {
+  $('.grid').one('click', function (e) {
+    if (turn===0) {
+      $(this).text(playerX);
+      boardCheck();
+      checkWin();
+      turn = 1;
+    } else {
+      $(this).text(playerO);
+      boardCheck();
+      checkWin();
+      turn = 0;
+    }
+  });
 }
 
-if (playerX.name != null) {
-  document.getElementById('playerXName').innerText = "Player X:      " + playerX.name;
-}
+//Initializes Game
+$(document).ready(
+  function xInfo() {
+    // setting up information input for Player X
+    var xName = prompt("Please enter your name", "Player X");
 
-
-
+    if (xName != null) {
+        document.getElementById("playerXName").innerHTML = xName + "     using X's";
+    }
+  });
 // setting up name input for Player O
-var playerO = {
-  name: prompt("Please enter your name, Player O"),
-  score: 0,
-  win: 0,
-  lose: 0
-}
+$(document).ready(
+  function oInfo (){
+    var oName = prompt("Please enter your name, Player O", "Player O");
 
-if (playerO.name != null) {
-  document.getElementById('playerOName').innerText = "Player O:     " + playerO.name;
-}
+  if (oName != null) {
+    document.getElementById("playerOName").innerHTML = oName + "     using O's";
+  }
 });
 
-
-// fleshing out the game board
+// determines if the grid is filled or not
+gridFill = function () {
+    a1 = $('#zero').html();
+    a2 = $('#one').html();
+    a3 = $('#two').html();
+    b1 = $('#three').html();
+    b2 = $('#four').html();
+    b3 = $('#five').html();
+    c1 = $('#six').html();
+    c2 = $('#seven').html();
+    c3 = $('#eight').html();
+};
