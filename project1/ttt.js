5d145da4119af9e6baf5a6850a43b25d331bec86
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-09-25 09:32:34 -0400

    Finn the glorious assisted me in getting things done in javascript functionality vastly improved

--- project1/ttt.js
+++ project1/ttt.js
@@ -16,158 +16,131 @@ $(document).ready(
   if (oName != null) {
     document.getElementById("playerOName").innerHTML = oName + "     using O's";
   }
+
 });
 
 
 // x's in the grid
 function xPlay () {
   $("#zero").click(function() {
-    $(document).ready(function(){
-        $("#zero").click(function(){
-            $("#zero").text('X').html();
-        })
-    })
-});
-$("#one").click(function() {
-    $(document).ready(function(){
-        $("#one").click(function(){
-            $("#one").text('X').html();
-        })
-    })
-});
-$("#two").click(function() {
-    $(document).ready(function(){
-        $("#two").click(function(){
-            $("#two").text('X').html();
-        })
-    })
-});
-$("#three").click(function() {
-    $(document).ready(function(){
-        $("#three").click(function(){
-            $("#three").text('X').html();
-        })
-    })
-});
-$("#four").click(function() {
-    $(document).ready(function(){
-        $("#four").click(function(){
-            $("#four").text('X').html();
-        })
-    })
-});
-$("#five").click(function() {
-    $(document).ready(function(){
-        $("#five").click(function(){
-            $("#five").text('X').html();
-        })
-    })
-});
-$("#six").click(function() {
-    $(document).ready(function(){
-        $("#six").click(function(){
-            $("#six").text('X').html();
-        })
-    })
-});
-$("#seven").click(function() {
-    $(document).ready(function(){
-        $("#seven").click(function(){
-            $("#seven").text('X').html();
-        })
-    })
-});
-$("#eight").click(function() {
-    $(document).ready(function(){
-        $("#eight").click(function(){
-            $("#eight").text('X').html();
-        })
-    })
-});
-}
+      $("#zero").text('X').html();
+        playerXTurn = false;
+        playerOTurn = true;
+        gameDisplay();
+  });
+  $("#one").click(function() {
+      $("#one").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#two").click(function() {
+      $("#two").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#three").click(function() {
+      $("#three").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#four").click(function() {
+      $("#four").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#five").click(function() {
+      $("#five").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#six").click(function() {
+      $("#six").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#seven").click(function() {
+      $("#seven").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
+  $("#eight").click(function() {
+      $("#eight").text('X').html();
+      playerXTurn = false;
+      playerOTurn = true;
+      gameDisplay();
+  });
 
 // o's in the grid
-function oPlay () {
-$("#zero").click(function() {
-    $(document).ready(function(){
-        $("#zero").click(function(){
-            $("#zero").text('O').html();
-        })
-    })
-});
-$("#one").click(function() {
-    $(document).ready(function(){
-        $("#one").click(function(){
-            $("#one").text('O').html();
-        })
-    })
-});
-$("#one").click(function() {
-    $(document).ready(function(){
-        $("#one").click(function(){
-            $("#one").text('O').html();
-        })
-    })
-});
-$("#two").click(function() {
-    $(document).ready(function(){
-        $("#two").click(function(){
-            $("#two").text('O').html();
-        })
-    })
-});
-$("#three").click(function() {
-    $(document).ready(function(){
-        $("#three").click(function(){
-            $("#three").text('O').html();
-        })
-    })
-});
-$("#four").click(function() {
-    $(document).ready(function(){
-        $("#four").click(function(){
-            $("#four").text('O').html();
-        })
-    })
-});
-$("#five").click(function() {
-    $(document).ready(function(){
-        $("#five").click(function(){
-            $("#five").text('O').html();
-        })
-    })
-});
-$("#six").click(function() {
-    $(document).ready(function(){
-        $("#six").click(function(){
-            $("#six").text('O').html();
-        })
-    })
-});
-$("#seven").click(function() {
-    $(document).ready(function(){
-        $("#seven").click(function(){
-            $("#seven").text('O').html();
-        })
-    })
-});
-$("#eight").click(function() {
-    $(document).ready(function(){
-        $("#eight").click(function(){
-            $("#eight").text('O').html();
-        })
-    })
-})
-};
+function OPlay () {
+  $("#zero").click(function() {
+      $("#zero").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#one").click(function() {
+      $("#one").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#two").click(function() {
+      $("#two").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#three").click(function() {
+      $("#three").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#four").click(function() {
+      $("#four").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#five").click(function() {
+      $("#five").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#six").click(function() {
+      $("#six").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#seven").click(function() {
+      $("#seven").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
+  $("#eight").click(function() {
+      $("#eight").text('O').html();
+      playerXTurn = true;
+      playerOTurn = false;
+      gameDisplay();
+  });
 
 // takin' turns budday
-var playerX = 1;
-function gameDisplay() {
-  if (playerX == 1){
-    document.getElementById('grid').text('X').html;
-    playerX = 0
-  } else {
-    document.getElementById('grid').text('0').html;
-    playerX = 1;
+var playerXTurn = true;
+var playerOTurn = false;
+function gameDisplay () {
+  if (playerX){
+     xPlay();
+  } else if (playerO) {
+     oPlay();
   }
-}
+};
