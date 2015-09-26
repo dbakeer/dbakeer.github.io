d5559962fa9da8da97a578a6c728156fc50e8891
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-09-26 14:39:40 -0400

    going the long way and hardcoding

--- project1/ttt.js
+++ project1/ttt.js
@@ -149,8 +149,8 @@ $(document).ready(
   }
 
   // determine a if a grid is filled
-  var grid = [];
-  var gridFill = function () {
+var grid = [];
+function gridFill() {
       grid[0] = $('#zero').html();
       grid[1] = $('#one').html();
       grid[2] = $('#two').html();
@@ -160,26 +160,64 @@ $(document).ready(
       grid[6] = $('#six').html();
       grid[7] = $('#seven').html();
       grid[8] = $('#eight').html();
-  };
+  }
 
 // list out winning grid combinations in an array
 var winCombo = [
-  [0 , 1 , 2],
-  [0 , 3 , 6],
-  [0 , 4 , 8],
-  [1 , 4 , 7],
-  [2 , 4 , 6],
-  [2 , 5 , 8],
-  [3 , 4 , 5],
-  [6 , 7 , 8]
+  [grid[0], grid[1], grid[3]],
+  [grid[0] , grid[1] , grid[2]],
+  [grid[0] , grid[3] , grid[6]],
+  [grid[0] , grid[4] , grid[8]],
+  [grid[1] , grid[4] , grid[7]],
+  [grid[2] , grid[4] , grid[6]],
+  [grid[2] , grid[5] , grid[8]],
+  [grid[3] , grid[4] , grid[5]],
+  [grid[6] , grid[7] , grid[8]]
 ];
 
 // determine the winner
-// xWinner = false;
-// oWinner = false;
-// function determineWinner(){
+xWinner = false;
+oWinner = false;
+function determineWinner(){
+    if ((a1 == a2 && a1 == a3 && (a1 == 'X')) ||
+    (b1 == b2 && b1 == b3 && (b1 == 'X')) ||
+    (c1 == c2 && c1 == c3 && (c1 == 'X')) ||
+    (a1 == b1 && a1 == c1 && (a1 == 'X')) ||
+    (a2 == b2 && a2 == c2 && (a2 == 'X')) ||
+    (a3 == b3 && a3 == c3 && (a3 == 'X')) ||
+    (a1 == b2 && a1 == c3 && (a1 == 'X')) ||
+    (a3 == b2 && a3 == c1 && (a3 == 'X'))
+    ) {
+        xWin = true;
+        winAlert();
 
-// checking to see if player X is the winner
+    } else { // CHECKS IF O WON
+        if ((a1 == a2 && a1 == a3 && (a1 == "o")) || //first row
+        (b1 == b2 && b1 == b3 && (b1 == "o")) || //second row
+        (c1 == c2 && c1 == c3 && (c1 == "o")) || //third row
+        (a1 == b1 && a1 == c1 && (a1 == "o")) || //first column
+        (a2 == b2 && a2 == c2 && (a2 == "o")) || //second column
+        (a3 == b3 && a3 == c3 && (a3 == "o")) || //third column
+        (a1 == b2 && a1 == c3 && (a1 == "o")) || //diagonal 1
+        (a3 == b2 && a3 == c1 && (a3 == "o")) //diagonal 2
+        ) {
+            oWin = true;
+            winAlert();
+
+        } else { // CHECKS FOR TIE GAME IF ALL CELLS ARE FILLED
+            if (((a1 == "x") || (a1 == "o")) && ((b1 == "x") || (b1 == "o")) && ((c1 == "x") || (c1 == "o")) && ((a2 == "x") || (a2 == "o")) && ((b2 == "x") || (b2 == "o")) && ((c2 == "x") || (c2 == "o")) && ((a3 == "x") || (a3 == "o")) && ((b3 == "x") || (b3 == "o")) && ((c3 == "x") || (c3 == "o"))) {
+                alert("It's a tie!");
+            }
+        }
+    }
+};
+
+
+
+
+
+
+
 
 // for (i = 0; i < winCombo.length; i++){
 //
