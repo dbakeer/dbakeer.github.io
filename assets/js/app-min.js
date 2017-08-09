012c520ac4a79d98e84d18d264bc60dbf6dd436f
dbakeer <dinah.bakeer@gmail.com>
2017-08-09 00:38:57 -0700

    updated

---
+++ assets/js/app-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".element").typed({strings:["Hello, my name is Dinah Bakeer.","I am a web developer and copywriter.","Thank you so much for visiting, and have an amazing day!"],typeSpeed:3})});
